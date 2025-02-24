74553b91e4de55e17d2e338122c3fa4b9ac88b09
DeborahOlaboye <deboraholaboye@gmail.com>
2025-02-24 11:19:16 +0100

    Included the mobile version of the app

--- script.js
+++ script.js
@@ -32,6 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
       prevButtons.forEach(button => {
         button.style.display = stepIndex === 0 ? "none" : "inline-block";
       });
+       // Update Sidebar
+        const sidebarSteps = document.querySelectorAll(".sidebar ul li");
+        sidebarSteps.forEach((li, index) => {
+            li.classList.toggle("active", index === stepIndex);
+    });
     }
   
     nextButtons.forEach(button => {
@@ -76,6 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
         toggleSwitch.classList.toggle("active");
         // updatePricing();
     });
+    
 
     // Plan pricing
     const planPrices = {
